448410aabf344133e10d5bf6b72f25a29f012e09
drtran <DrKietTran@gmail.com>
2018-07-26 05:17:52 -0400

    Added code to use location for the goodbye service

--- goodbye-service/src/main/java/com/drkiettran/goodbyeservice/GoodbyeServiceApplication.java
+++ goodbye-service/src/main/java/com/drkiettran/goodbyeservice/GoodbyeServiceApplication.java
@@ -3,6 +3,7 @@ package com.drkiettran.goodbyeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class GoodbyeServiceApplication {
 	}
 
 	@RequestMapping
-	public String goodbye() {
-		return "Goodbye!";
+	public String goodbye(@RequestHeader("x-location") String location) {
+		return "Goodbye from " + location + "!";
 	}
 }
